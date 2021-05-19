0c36a3c892863d70db547c900823c747604dd0c1
bcai2 <bcai@alumni.stanford.edu>
2021-05-19 10:40:02 -0500

    added key checking + test.py

--- src/query.py
+++ src/query.py
@@ -1,6 +1,19 @@
+import os
+
 import requests
 
-def query(**kwargs):
+def query(tool, version, **kwargs):
+    """
+    TODO: Document
+    """
+
+    try:
+        key = os.environ["TOOLCHEST_KEY"]
+    except KeyError:
+        print("Key not found. Please set env var TOOLCHEST_KEY to your specified Toolchest authentication key.")
+        return None
+
+
     pass
 
 def cutadapt(cutadapt_args, **kwargs):
