d105b8c0a90614c9e4f9a51269dcce81cffab794
Noah Lebovic <noah@lebovic.com>
2021-07-21 14:20:32 -0500

    Override built-in print statement to flush after every print

--- toolchest_client/__init__.py
+++ toolchest_client/__init__.py
@@ -1,9 +1,14 @@
 # TODO: include a docstring here
 
+import builtins
 from dotenv import load_dotenv, find_dotenv
+import functools
 
 # .env load must be before imports that use environment variables
 load_dotenv(find_dotenv(".env"))
+
+# specifying print flushing is necessary to support loading from R
+builtins.print = functools.partial(print, flush=True)
 
 from .auth import get_key, set_key
 from .exceptions import ToolchestException, DataLimitError
