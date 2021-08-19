6430cd8a6edeb02cb4b478d863bafe7c4a464552
bcai2 <bcai@alumni.stanford.edu>
2021-08-18 17:34:55 -0700

    added instance.run()

--- toolchest_client/tools/api.py
+++ toolchest_client/tools/api.py
@@ -142,6 +142,7 @@ def STAR(output_path, database_name, database_version="1", read_one=None, read_t
         database_name=database_name,
         database_version=database_version,
     )
+    instance.run()
 
 def test(inputs, output_path, tool_args=""):
     """Run a test pipeline segment via Toolchest. A plain text file containing 'success' is returned."
