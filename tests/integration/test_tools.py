dd919098963a1169877c112ccd12cbca2851d965
Noah Lebovic <lebovic@users.noreply.github.com>
2021-08-26 11:16:33 -0500

    Add pytest (#29)
    Adds:
    - `pytest` basics
    - One integration test – for kraken 2 – that pulls from S3 and verifies hash of the output
    - One very basic unit test

---
+++ tests/integration/test_tools.py
@@ -0,0 +1,35 @@
+import pytest
+
+import hash
+import os
+import s3
+
+if os.environ.get("DEPLOY_ENVIRONMENT") == "staging":
+    os.environ["BASE_URL"] = os.environ["TOOLCHEST_STAGING_URL"]
+
+import toolchest_client as toolchest
+
+toolchest_api_key = os.environ.get("TOOLCHEST_API_KEY")
+if toolchest_api_key:
+    toolchest.set_key(toolchest_api_key)
+
+
+@pytest.mark.integration
+def test_kraken2_standard():
+    """
+    Tests Kraken 2 against the standard (v1) database
+    """
+    input_file_path = "./kraken_input.fasta"
+    output_file_path = "./kraken_output.txt"
+
+    s3.download_integration_test_input(
+        s3_file_key="synthetic_bacteroides_reads.fasta",
+        output_file_path=input_file_path,
+    )
+
+    toolchest.kraken2(
+        inputs=input_file_path,
+        output_path=output_file_path,
+    )
+
+    assert hash.unordered(output_file_path) == 886254946
