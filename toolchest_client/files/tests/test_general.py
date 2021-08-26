27ab8c32f5600fec297092ef14e6875ec1cc7e9a
bcai2 <bcai@alumni.stanford.edu>
2021-08-26 16:47:29 -0700

    added test_file_too_large(), test_exists_but_not_file()

--- toolchest_client/files/tests/test_general.py
+++ toolchest_client/files/tests/test_general.py
@@ -15,12 +15,19 @@ def test_small_file():
         sanity_check(small_file_path)
 
 def test_files_in_path():
+    # TODO: this
     pass
 
 def test_file_too_large():
-    pass
+    with pytest.raises(ValueError):
+        check_file_size(f"{THIS_FILE_PATH}/data/eight_line.fastq", max_size_bytes=100)
 
 def test_nonexistent_file():
     bogus_file_path = f"{THIS_FILE_PATH}/data/bogus_file_path"
     with pytest.raises(FileNotFoundError):
         assert_exists(bogus_file_path)
+
+def test_exists_but_not_file():
+    dir_file_path = f"{THIS_FILE_PATH}/data"
+    with pytest.raises(ValueError):
+        assert_exists(dir_file_path, must_be_file=True)
