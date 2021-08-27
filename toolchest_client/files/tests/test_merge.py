1f57fba174d81e6426b35dde862e7858931e2ebe
bcai2 <bcai@alumni.stanford.edu>
2021-08-27 15:16:45 -0700

    added todo comment

--- toolchest_client/files/tests/test_merge.py
+++ toolchest_client/files/tests/test_merge.py
@@ -16,7 +16,9 @@ def test_concatenate_files():
     temp_output_file_path = f"{THIS_FILE_PATH}/data/temp_output.fastq"
 
     concatenate_files([split_one_path, split_two_path], temp_output_file_path)
-    
+
     assert filecmp.cmp(input_file_path, temp_output_file_path)
 
     os.remove(temp_output_file_path)
+
+# TODO: test merge_sam_files() in a way that's reproducible on different OS choices
