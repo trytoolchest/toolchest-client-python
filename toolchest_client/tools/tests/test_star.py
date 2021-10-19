e942981e065f4bde2c2b37e5a0e975672ad19378
Noah Lebovic <noah@lebovic.com>
2021-10-18 19:30:57 -0500

    Remove unsupported test

--- toolchest_client/tools/tests/test_star.py
+++ toolchest_client/tools/tests/test_star.py
@@ -39,22 +39,3 @@ def test_star_variable_arg_parsing_multiple():
     star_instance._validate_args()
 
     assert star_instance.tool_args == "--quantMode TranscriptomeSAM GeneCounts --scoreGap 1"
-
-
-def test_removes_for_invalid_arg():
-    star_instance = STARInstance(
-        tool_args="--quantMode GeneCounts --badArg 0 --scoreGap 1",
-        output_name='Aligned.out.sam',
-        input_prefix_mapping={
-            "r1_path": None,
-            "r2_path": None,
-        },
-        inputs=f"{THIS_DIRECTORY}/test_star.py",
-        output_path="./output.sam",
-        database_name="test",
-        database_version="0.1.0",
-    )
-    star_instance._validate_args()
-
-    assert star_instance.tool_args == "--quantMode GeneCounts --scoreGap 1"
-
