a7e7e7b1756712a48956425b2d67f40fc550e974
Noah Lebovic <noah@lebovic.com>
2021-11-23 15:39:49 -0600

    Split tool integration tests

---
+++ tests/tool_integration/conftest.py
@@ -0,0 +1,11 @@
+import os
+
+
+def pytest_configure(_):
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    if os.environ.get("DEPLOY_ENVIRONMENT") == "staging":
+        os.environ["BASE_URL"] = os.environ["TOOLCHEST_STAGING_URL"]
