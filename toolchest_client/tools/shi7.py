8c70b15ee5e37ed94234bf88b2e6f76d39e042fa
Noah Lebovic <noah@lebovic.com>
2021-12-19 22:05:33 -0600

    Add final shi7 output

--- toolchest_client/tools/shi7.py
+++ toolchest_client/tools/shi7.py
@@ -27,5 +27,5 @@ class Shi7(Tool):
             max_input_bytes_per_file=5 * 1024 * 1024 * 1024,
             output_type=OutputType.GZ_TAR,
             output_is_directory=True,
-            output_names=["combined_seqs.fna"],
+            output_names=["combined_seqs.fna", "shi7.log"],
         )
