89c1fa3d4a4d320345fb59278faf1675ba3401e2
Justin Herr <justin@laytra.com>
2022-01-25 13:14:06 -0600

    fix: remove pysam and STAR parallelization for now

--- toolchest_client/tools/star.py
+++ toolchest_client/tools/star.py
@@ -30,7 +30,7 @@ class STARInstance(Tool):
             max_inputs=2,
             database_name=database_name,
             database_version=database_version,
-            parallel_enabled=True if parallelize else False,
+            parallel_enabled=False, # True if parallelize else False
             max_input_bytes_per_file=128 * 1024 * 1024 * 1024,
             max_input_bytes_per_file_parallel=4.5 * 1024 * 1024 * 1024,
             output_type=OutputType.SAM_FILE if parallelize else OutputType.GZ_TAR,
