aa661b4df91927c02446a0edc73dd25fcf4bd9c9
bcai2 <bcai@alumni.stanford.edu>
2022-02-23 13:53:50 -0800

    refactor: convert private check to flag

--- tests/util/s3.py
+++ tests/util/s3.py
@@ -4,12 +4,10 @@ from botocore.exceptions import ClientError
 
 # Downloads input from S3 to local file path.
 # Used for tests that upload local inputs.
-def download_integration_test_input(s3_file_key, output_file_path):
+def download_integration_test_input(s3_file_key, output_file_path, is_private=False):
     s3_client = boto3.client('s3')
+    bucket_name = 'toolchest-integration-tests-private' if is_private else 'toolchest-integration-tests'
     try:
-        s3_client.download_file('toolchest-integration-tests', s3_file_key, output_file_path)
-    except ClientError:
-        try:
-            s3_client.download_file('toolchest-integration-tests-private', s3_file_key, output_file_path)
-        except ClientError as err:
-            raise err
+        s3_client.download_file(bucket_name, s3_file_key, output_file_path)
+    except ClientError as err:
+        raise err
