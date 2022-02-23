aec07ca5b8b056cdfe07aab3a5fb4a17763e52c2
Bryce Cai <bcai@trytoolchest.com>
2022-02-23 14:32:24 -0800

    refactor: S3 download from fully private bucket (#109)
    * refactor: S3 download from fully private bucket
    
    * refactor: convert private check to flag
    
    * chore: cleanup extra try-catch
    
    * chore: cleanup stray import

--- tests/util/s3.py
+++ tests/util/s3.py
@@ -1,7 +1,9 @@
 import boto3
 
 
-# todo: once able to pass S3 paths as input, remove this ability
-def download_integration_test_input(s3_file_key, output_file_path):
-    s3 = boto3.client('s3')
-    s3.download_file('toolchest-integration-tests', s3_file_key, output_file_path)
+# Downloads input from S3 to local file path.
+# Used for tests that upload local inputs.
+def download_integration_test_input(s3_file_key, output_file_path, is_private=False):
+    s3_client = boto3.client('s3')
+    bucket_name = 'toolchest-integration-tests-private' if is_private else 'toolchest-integration-tests'
+    s3_client.download_file(bucket_name, s3_file_key, output_file_path)
