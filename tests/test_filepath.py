482cbdb821a76cd80551944dc25523f0fbe757e3
bcai2 <bcai@alumni.stanford.edu>
2022-04-14 14:52:49 -0700

    chore: remove duplicate variable

--- tests/test_filepath.py
+++ tests/test_filepath.py
@@ -14,11 +14,10 @@ def test_tilde_filepath():
     """
     Tests Kraken 2 against the standard (v1) database
     """
-    test_dir = "~/test_tilde_filepath"
-    os.makedirs(os.path.expanduser(test_dir), exist_ok=True)
     input_file_path = "~/kraken_input.fasta"
-    output_dir_path = f"{test_dir}"
+    output_dir_path = "~/test_tilde_filepath"
     output_file_path = os.path.expanduser(f"{output_dir_path}/kraken2_output.txt")
+    os.makedirs(os.path.expanduser(output_dir_path), exist_ok=True)
 
     s3.download_integration_test_input(
         s3_file_key="synthetic_bacteroides_reads.fasta",
