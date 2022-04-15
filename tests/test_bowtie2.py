65c7c20e63f7198d86d82b8aea7a43d3059fef27
jherr-dev <jherr@trytoolchest.com>
2022-04-15 16:21:00 -0500

    feat: add integration test for bowtie2 (#139)
    Co-authored-by: Justin Herr <justin@laytra.com>

---
+++ tests/test_bowtie2.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+
+from tests.util import hash
+import toolchest_client as toolchest
+
+toolchest_api_key = os.environ.get("TOOLCHEST_API_KEY")
+if toolchest_api_key:
+    toolchest.set_key(toolchest_api_key)
+
+DEFAULT_OUTPUT_HASH = 1557898163
+
+
+@pytest.mark.integration
+def test_bowtie2():
+    """
+    Tests bowtie2
+    """
+
+    test_dir = "test_bowtie2_standard"
+    os.makedirs(f"./{test_dir}", exist_ok=True)
+    output_dir_path = f"./{test_dir}/"
+
+    toolchest.diamond_blastp(
+        inputs="s3://toolchest-integration-tests/sample_r1.fastq.gz",
+        output_path=output_dir_path,
+    )
+
+    assert hash.unordered(f"{output_dir_path}/bowtie2_output.sam") == DEFAULT_OUTPUT_HASH
