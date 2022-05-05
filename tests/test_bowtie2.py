570a46dcd0f2e792e7c4fd3c02c1a288e01edd8f
jherr-dev <jherr@trytoolchest.com>
2022-05-05 16:07:37 -0500

    fix: use correct test file in bowtie2 test (#159)

--- tests/test_bowtie2.py
+++ tests/test_bowtie2.py
@@ -19,7 +19,7 @@ def test_bowtie2():
     output_dir_path = f"./{test_dir}/"
 
     toolchest.bowtie2(
-        inputs="s3://toolchest-integration-tests/sample_r1.fastq.gz",
+        inputs="s3://toolchest-integration-tests/DRR000006.fastq.gz",
         output_path=output_dir_path,
     )
 
