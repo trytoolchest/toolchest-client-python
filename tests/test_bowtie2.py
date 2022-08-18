a806790424fcd98b2413d2c22743f7b541771d8f
bcai2 <29902109+bcai2@users.noreply.github.com>
2022-08-18 12:19:57 -0700

    fix: bowtie2 test

--- tests/test_bowtie2.py
+++ tests/test_bowtie2.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 
+from tests.util import filter_output, hash
 import toolchest_client as toolchest
 
 toolchest_api_key = os.environ.get("TOOLCHEST_API_KEY")
@@ -17,11 +18,14 @@ def test_bowtie2():
     test_dir = "temp_test_bowtie2_standard"
     os.makedirs(f"./{test_dir}", exist_ok=True)
     output_dir_path = f"./{test_dir}/"
+    output_file_path = f"{output_dir_path}bowtie2_output.sam"
+    filtered_output_file_path = f"{output_dir_path}bowtie2_output.filtered.sam"
 
     toolchest.bowtie2(
         inputs="s3://toolchest-integration-tests/DRR000006.fastq.gz",
         output_path=output_dir_path,
     )
 
-    # Bowtie2 hash is non-deterministic but size is consistent between runs
-    assert os.path.getsize(f"{output_dir_path}bowtie2_output.sam") == 1043855350
+    # Filter non-deterministic metadata lines
+    filter_output.filter_sam(output_file_path, filtered_output_file_path)
+    assert hash.unordered(filtered_output_file_path) == 1444969892
