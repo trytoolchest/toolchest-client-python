d2f23c56ef98434a1403bdfed7506ab54a9bd4d3
bcai2 <29902109+bcai2@users.noreply.github.com>
2022-09-15 17:06:42 -0700

    fix: metaphlan test paths

--- tests/test_metaphlan.py
+++ tests/test_metaphlan.py
@@ -25,12 +25,14 @@ def test_metaphlan():
     )
 
     # MetaPhLan includes the command in the output but that contains a non-deterministic uuid so that line is removed.
+    filtered_output_path = os.path.join(output_dir_path, "out_filtered.txt")
     filter_output.filter_regex(
-        unfiltered_path="/Users/justinherr/temp/test/metaphlan/out.txt",
-        filtered_path="/Users/justinherr/temp/test/metaphlan/out_filtered.txt",
+        unfiltered_path=f"{output_dir_path}/out.txt",
+        filtered_path=filtered_output_path,
         search_regex="#/usr/local/bin/metaphlan.*\n",
         replacement_str="",
     )
+    assert hash.unordered(filtered_output_path) == 1401032462
 
-    assert hash.unordered(os.path.join(test_dir, "out.txt")) == 1401032462
-    assert hash.unordered(os.path.join(test_dir, "SRS014464-Anterior_nares.fasta.gz.bowtie2out.txt")) == 1308716263
+    bowtie2outfile_path = os.path.join(output_dir_path, "SRS014464-Anterior_nares.fasta.gz.bowtie2out.txt")
+    assert hash.unordered(bowtie2outfile_path) == 1308716263
