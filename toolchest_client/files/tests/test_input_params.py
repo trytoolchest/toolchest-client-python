76db008bcdd12e12658e98b028996fbb916db5e3
bcai2 <29902109+bcai2@users.noreply.github.com>
2022-10-28 16:14:48 -0700

    chore: fix unit test

--- toolchest_client/files/tests/test_input_params.py
+++ toolchest_client/files/tests/test_input_params.py
@@ -4,10 +4,18 @@ from .. import convert_input_params_to_prefix_mapping
 def test_generate_prefix_mapping():
     tag_to_param_map = {
         "-1": ["example1_R1.fastq", "example2_R1.fastq"],
-        "-2": ["example1_R2.fastq", "example2_R1.fastq"],
+        "-2": ["example1_R2.fastq", "example2_R2.fastq"],
         "-U": ["example1_U.fastq", "example2_U.fastq"],
     }
-    prefix_mapping = convert_input_params_to_prefix_mapping(tag_to_param_map)
+    input_list, prefix_mapping = convert_input_params_to_prefix_mapping(tag_to_param_map)
+    assert sorted(input_list) == sorted([
+        "example1_R1.fastq",
+        "example2_R1.fastq",
+        "example1_R2.fastq",
+        "example2_R2.fastq",
+        "example1_U.fastq",
+        "example2_U.fastq",
+    ])
     assert prefix_mapping == {
         "example1_R1.fastq": {
             "prefix": "-1",
